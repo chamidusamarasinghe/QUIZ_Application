64d65c6bd7beacdcc900610b5c8492ef630614c2
S.A.C.Jayanga <148344607+chamidusamarasinghe@users.noreply.github.com>
2025-11-20 10:08:48 +0530

    Update MongoDB connection string in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 //create connection with mongodb & create database
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://userquiz:quizapp@cluster0.mdxevfq.mongodb.net/QuizApp')
+    await mongoose.connect('connection_String')
     .then(() => {
         console.log("MongoDB connected successfully");
     })
